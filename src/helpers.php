21749b1062b2c333912fefefd091c7fa5bc5e25a
Lance <lamon0624@gmail.com>
2019-09-05 16:50:18 +0800

    update helpers

---
+++ src/helpers.php
@@ -0,0 +1,47 @@
+<?php
+
+if (!function_exists('admin_url')) {
+    /**
+     * Get admin url.
+     *
+     * @param string $path
+     * @param mixed  $parameters
+     * @param bool   $secure
+     *
+     * @return string
+     */
+    function admin_url($path = '', $parameters = [], $secure = null)
+    {
+        if (\Illuminate\Support\Facades\URL::isValidUrl($path)) {
+            return $path;
+        }
+
+        $secure = $secure ?: (config('admin.https') || config('admin.secure'));
+
+        return url(admin_base_path($path), $parameters, $secure);
+    }
+}
+
+if (!function_exists('admin_base_path')) {
+    /**
+     * Get admin url.
+     *
+     * @param string $path
+     *
+     * @return string
+     */
+    function admin_base_path($path = '')
+    {
+        $prefix = '/'.trim(config('admin.route.prefix'), '/');
+
+        $prefix = ($prefix == '/') ? '' : $prefix;
+
+        $path = trim($path, '/');
+
+        if (is_null($path) || strlen($path) == 0) {
+            return $prefix ?: '/';
+        }
+
+        return config('itheatre.short_url_name').'/'.$prefix.'/'.$path;
+    }
+}
